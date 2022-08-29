dbf4223aed6c7394f66944f91a4852837a944400
Aman <rawataman101@gmail.com>
2022-08-29 12:48:49 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App/>, document.getElementById("root"));
