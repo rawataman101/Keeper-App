dc53aff6a63900a49762d59ec38098875f97392c
Aman <rawataman101@gmail.com>
2022-09-06 22:13:00 +0530

    Updates

---
+++ precache-manifest.0109804f4fb002b2e1ec29d7160ba219.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562d5a45ca1118c4cdf558bd7ddc8685",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "888a93ef210832f3ecdd",
+    "url": "/Keeper-App/static/js/2.aa3d6807.chunk.js"
+  },
+  {
+    "revision": "fd179bf675c57cae174b",
+    "url": "/Keeper-App/static/js/main.840a03a3.chunk.js"
+  },
+  {
+    "revision": "3d8946ade4a807233ed6",
+    "url": "/Keeper-App/static/js/runtime-main.9737ed9f.js"
+  }
+]);
